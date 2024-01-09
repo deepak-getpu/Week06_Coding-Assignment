4e34236339fd3f03f3d619e92462c049d68d2d36
deepak-getpu <deepak.getpu@gmail.com>
2024-01-09 08:29:04 -0500

    Revised the main class code.

--- src/week06CodingAssignment/App.java
+++ src/week06CodingAssignment/App.java
@@ -18,25 +18,30 @@ public class App {
 		players.add(p1);
 		players.add(p2);
 		
-		for (int i=1; i<=52; i++) {	
+		for (int i=1; i<=26; i++) {	
 			for (Player player : players) {
 				player.drawCard(deck);
 			}
 		}
 
 		for (int i=1; i<=26; i++) {
-			p1.flipCard();
-			p2.flipCard(); 
-			if ((p1.flipCard().getValue()) > p2.flipCard().getValue()) {
+			System.out.println("Round: " + i);
+			Card p1Draw = p1.flipCard();
+			Card p2Draw = p2.flipCard(); 
+			
+			if ((p1Draw.getValue()) > p2Draw.getValue()) {
 				p1.incrementScore();
 				System.out.println("P1 got the point.");
-			} else if ((p1.flipCard().getValue()) < p2.flipCard().getValue()) {
+				
+			} else if ((p1Draw.getValue()) < p2Draw.getValue()) {
 				p2.incrementScore();
-				System.out.println("P2 got the point");
+				System.out.println("P2 got the point.");
 			} else {
 				System.out.println("No one got the point.");
 			}
-			
+			System.out.println("Player 1 score " + p1.getScore());
+			System.out.println("Player 2 score " + p2.getScore());
+			System.out.println();
 		}
 		
 		int a = p1.getScore();
@@ -44,14 +49,16 @@ public class App {
 		
 		if (a>b) {
 			System.out.println("Player 1 won");
-			System.out.println("Score: " + a);
+			System.out.println("Player 1 final score " + p1.getScore());
+			System.out.println("Player 2 final score " + p2.getScore());
 		} else if (a < b) {
 			System.out.println("Player 2 won");
-			System.out.println("Score: " + b);
+			System.out.println("Player 1 final score " + p1.getScore());
+			System.out.println("Player 2 final score " + p2.getScore());
 		} else {
 			System.out.println("No one won!");
-		    System.out.println("Score for P1: " + a);
-		    System.out.println("Score for P2: " + b);
+			System.out.println("Player 1 final score " + p1.getScore());
+			System.out.println("Player 2 final score " + p2.getScore());
 		}
 	
 
